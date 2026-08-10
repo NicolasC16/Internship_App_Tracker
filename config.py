16a16acce0db8a002f0bfb67e991dfd66b6e22bc
Nicolas Capetillo <nicapeti@ttu.edu>
2026-08-10 04:07:25 -0500

    Update Configuration File

--- config.py
+++ config.py
@@ -6,11 +6,10 @@ load_dotenv()
 YAHOO_EMAIL = os.environ["YAHOO_EMAIL"]
 YAHOO_APP_PASSWORD = os.environ["YAHOO_APP_PASSWORD"]
 
-OPEN_AI_KEY = os.environ["OPEN_AI_KEY"]
-
 YAHOO_IMAP_SERVER = "imap.mail.yahoo.com"
 YAHOO_IMAP_PORT = 993
 
 DATABASE_FILE = "internships.db"
 
-INITIAL_SYNC_DAYS = 90
+# How many recent emails to process
+INITIAL_SYNC_DAYS = 50
