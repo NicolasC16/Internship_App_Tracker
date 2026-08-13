0f77028796226e5fe88942ddaa6b9aad4309335e
Nicolas Capetillo <nicapeti@ttu.edu>
2026-08-13 17:26:28 -0500

    Add comment lines

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
+""" This file is responsible for configuring the information needed to communicate with Yahoo."""
+
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()                                               #Loads the .env file
 
-YAHOO_EMAIL = os.environ["YAHOO_EMAIL"]
-YAHOO_APP_PASSWORD = os.environ["YAHOO_APP_PASSWORD"]
+YAHOO_EMAIL = os.environ["YAHOO_EMAIL"]                     # Gets my yahoo email address from .env
+YAHOO_APP_PASSWORD = os.environ["YAHOO_APP_PASSWORD"]       # Gets my yahoo app password from .env
 
-YAHOO_IMAP_SERVER = "imap.mail.yahoo.com"
-YAHOO_IMAP_PORT = 993
+YAHOO_IMAP_SERVER = "imap.mail.yahoo.com"                   # Store's Yahoo's IMAP server address
+YAHOO_IMAP_PORT = 993                                       # Stores the port used for IMAP
 
-DATABASE_FILE = "internships.db"
+DATABASE_FILE = "internships.db"                            # Sets the name for the SQLite database file
 
 # How many recent emails to process
-INITIAL_EMAIL_LIMIT = 50
+INITIAL_EMAIL_LIMIT = 50                                    
--- yahoo_mail.py
+++ yahoo_mail.py
@@ -1,6 +1,11 @@
+"""This file is responsible for controlling the connection and 
+communication with Yahoo. It does not decode or evaluate the information within an email.
+Instead it takes the raw data from an email and shares it to the rest of the program
+for the data to be decoded and stored."""
+
 import imaplib
 
-from config import (
+from config import (                                # Import Yahoo configuration values from config.py
     YAHOO_EMAIL,
     YAHOO_APP_PASSWORD,
     YAHOO_IMAP_SERVER,
@@ -13,70 +18,70 @@ class YahooMail:
 
         self.mail = None
 
-    def connect(self):
+    def connect(self):                              # Creates a function used to connect to Yahoo
 
         print("Connecting to Yahoo")
 
-        self.mail = imaplib.IMAP4_SSL(
+        self.mail = imaplib.IMAP4_SSL(              # Connects to Yahoo through it's server and port
             YAHOO_IMAP_SERVER,
             YAHOO_IMAP_PORT
         )
 
-        self.mail.login(
+        self.mail.login(                            # Logs into Yahoo using my email and app password
             YAHOO_EMAIL,
             YAHOO_APP_PASSWORD
         )
 
-        self.mail.select("INBOX")
+        self.mail.select("INBOX")                   # Selects the inbox folder for email evaluation
 
         print("Connected to Yahoo.")
 
-    def get_recent_emails(self, limit=50):
+    def get_recent_emails(self, limit=50):          # Function used for getting recent emails
 
-        status, messages = self.mail.search(
+        status, messages = self.mail.search(        # Searches all of the emails within the limit
             None,
             "ALL"
         )
 
-        if status != "OK":
+        if status != "OK":                          # If the search fails then return an empty list
             return[]
 
-        email_ids = messages[0].split()
+        email_ids = messages[0].split()             # Splits the returned bytes containing the IDs
 
         #Get only the newest emails
-        email_ids = email_ids[-limit:]
+        email_ids = email_ids[-limit:]              # -limit means start 50 emails from the end then go to the end
 
         emails = []
 
-        for email_id in email_ids:
+        for email_id in email_ids:                  # Goes through each email ID
 
-            status, data = self.mail.fetch(
+            status, data = self.mail.fetch(         # RFC822 asks for the entire email
                 email_id,
                 "(RFC822)"
             )
 
-            if status != "OK":
+            if status != "OK":                      # If retrieval of a specific email fails, move on to the next
                 continue
 
-            raw_email = data[0][1]
+            raw_email = data[0][1]                  # Pulls the raw email bytes out of Yahoo's response
 
-            emails.append({
+            emails.append({                         # Adds a dictionary that stores the email id and raw email to the emails list
                 "id": email_id.decode(),
                 "raw": raw_email
             })
 
-        return emails
+        return emails                               # Returns the emails list
 
-    def close(self):
+    def close(self):                                # Defines the function that closes and ends the communication with Yahoo
 
-        if self.mail:
+        if self.mail:                               # Check for a connection with Yahoo
 
-            try:
+            try:                                    # Closes the selected mailbox        
                 self.mail.close()
             except Exception:
                 pass
 
-            try:
+            try:                                    # Logs out of Yahoo
                 self.mail.logout()
             except Exception:
                 pass
